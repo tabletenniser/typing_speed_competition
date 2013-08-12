dc388ad5db9efb81073da5b97302965b993cfc66
unknown <tabletenniser@gmail.com>
2013-08-12 03:35:47 -0400

    font_size_adjustment

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"');
 require_once('AppInfo.php');	// contains appID, SECRET and URL
 
+	
+  echo "begin!!!";
+	
 // Enforce https on production
 if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
 header('Location: https://google.com');
